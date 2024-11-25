663ce3f8497355dbf2393463b6bf8fab4e52b7f0
Ján Plachý <jan.plachy946@student.cuni.cz>
2024-11-25 17:48:26 +0100

    Add reverse complements into ILP, canonical printing of masked output

--- src/ilp/distance_functions.h
+++ src/ilp/distance_functions.h
@@ -13,7 +13,7 @@
 #include "../kmers.h"
 
 template<typename kmer_t>
-size_t compute_overlap(kmer_t kmer1, kmer_t kmer2, size_t k){
+size_t compute_max_overlap(kmer_t kmer1, kmer_t kmer2, size_t k){
     for (size_t ov = k; ov > 0; --ov){
         bool found = true;
         for (size_t i = 0; i < ov; ++i){
@@ -33,5 +33,5 @@ int trivial_distance(const std::vector<kmer_t>& kmers, size_t k, size_t index1, 
         throw std::invalid_argument("index " + std::to_string(std::max(index1, index2)) + " outside of bounds [0, " + std::to_string(kmers.size() - 1) + "]");
     
     kmer_t kmer1 = kmers[index1], kmer2 = kmers[index2];
-    return k - compute_overlap(kmer1, kmer2, k);
+    return k - compute_max_overlap(kmer1, kmer2, k);
 }
