ebb44657a1ef4bed560e04557081274bf938fd42
Ján Plachý <jan.plachy946@student.cuni.cz>
2024-12-09 01:01:41 +0100

    Add first draft of HOG construction, some other changes (still not tested)

--- src/hog/hog.h
+++ src/hog/hog.h
@@ -53,10 +53,9 @@ template<typename size_t_max>
 struct Node {
     size_t_max kmer_index;
     size_t_max depth;
-    size_t_max index; // Might not need it in the end
+    //size_t_max index; // Might not need it in the end
     size_t_max parent;
     size_t_max failure;
-    bool has_basic_child[4]; // Only used when constructing AC
     std::vector<size_t_max> children;
 
     Node(size_t_max kmer_index, size_t_max depth, size_t_max index, size_t_max parent) :
@@ -76,7 +75,7 @@ struct HOGConstructer {
     //constexpr size_t_max INVALID_NODE = std::numeric_limits<size_t_max>::max();
     //static bool is_node_valid(size_t_max node_index) { return node_index != INVALID_NODE};
 
-    short get_edge_nucleotide(size_t_max child_index){
+    uint8_t get_edge_nucleotide(size_t_max child_index){
         return NucleotideIndexAtIndex(kMers[nodes[child_index].kmer_index], k, nodes[child_index].depth);
     }
 
@@ -84,13 +83,54 @@ struct HOGConstructer {
         k(k), n(0) {};
     HOGConstructer(size_t_max k, std::vector<kmer_t>& kMers) :
         k(k), kMers(kMers), n(kMers.size()) {};
+
+    void remove_redundant_edges(std::vector<bool>& keep);
+
     void construct_AC();
     void convert_AC_to_EHOG();
     void convert_EHOG_to_HOG();
 };
 
+
+// Remove non-keeped nodes
 template <typename kmer_t, typename size_t_max>
-inline void HOGConstructer<kmer_t, size_t_max>::construct_AC() {
+inline void HOGConstructer<kmer_t, size_t_max>::remove_redundant_edges(std::vector<bool>& keep)
+{
+    size_t_max node_count = nodes.size();
+    if (node_count != keep.size()) throw std::invalid_argument("Invalid removing mask passed as argument");
+
+    // Move keeped nodes to the beginning
+    std::vector<size_t_max> new_indexes(node_count, 0);
+    size_t_max keeped = 0;
+    for (size_t_max i = 0; i < node_count; ++i){
+        if (keep[i]){
+            std::swap(nodes[i], nodes[keeped++]);
+            new_indexes[i] = keeped - 1;
+        }
+    }
+
+    // Set all the edges to point to correct locations
+    for (size_t_max i = 0; i < keeped; ++i){
+        //nodes[i].index = new_indexes[i];
+        nodes[i].parent = new_indexes[nodes[i].parent];
+        nodes[i].failure = new_indexes[nodes[i].failure];
+        
+        size_t_max child_count = nodes[i].children.size();
+        for (size_t_max j = 0; j < child_count; ++j)
+            nodes[i].children[j] = new_indexes[nodes[i].children[j]];
+    }
+
+    // Set correct pointers from leaves and BFS_indexes
+    for (size_t_max i = 0; i < n; ++i) leaves[i] = new_indexes[leaves[i]];
+    for (size_t_max i = 0; i < keeped; ++i) BFS_indexes[i] = new_indexes[BFS_indexes[i]];
+    
+    // Get rid of non-keeped nodes
+    nodes.resize(keeped);
+}
+
+template <typename kmer_t, typename size_t_max>
+inline void HOGConstructer<kmer_t, size_t_max>::construct_AC()
+{
     if (!nodes.empty()) throw std::invalid_argument("AC has already been constructed");
 
     // Add nodes and forward edges
@@ -100,16 +140,18 @@ inline void HOGConstructer<kmer_t, size_t_max>::construct_AC() {
 
     leaves.resize(n);
 
+    std::vector<bool> has_basic_child(4 * node_count, false);
+
     for (size_t_max kmer_index = 0; kmer_index < n; ++kmer_index){
         size_t_max node_index = 0;
         for (size_t_max pos = 0; pos < k; ++pos){
-            short nucleotide_index = NucleotideIndexAtIndex(kMers[kmer_index], k, pos);
-            if (nodes[node_index].has_basic_child[nucleotide_index]){
+            uint8_t nucleotide_index = NucleotideIndexAtIndex(kMers[kmer_index], k, pos);
+            if (has_basic_child[nodes[node_index] * 4 + nucleotide_index]){
                 // Position of new node = node count
                 nodes.emplace_back<Node<size_t_max>>(kmer_index, pos + 1, node_count, node_index);
                 // Add new child to parent
                 nodes[node_index].children.push_back(node_count);
-                nodes[node_index].has_basic_child[get_edge_nucleotide(node_count)] = true;
+                has_basic_child[nodes[node_index] * 4 + get_edge_nucleotide(node_count)] = true;
                 // Increase node count
                 node_count++;
 
@@ -122,11 +164,7 @@ inline void HOGConstructer<kmer_t, size_t_max>::construct_AC() {
     }
 
     // Add fake goto links to root
-    for (short i = 0; i < 4; ++i){
-        if (!nodes[0].has_basic_child[i]){
-            nodes[0].has_basic_child[i];
-        }
-    }
+    for (uint8_t i = 0; i < 4; ++i) has_basic_child[i] = true;
 
     BFS_indexes.resize(node_count);
 
@@ -140,11 +178,11 @@ inline void HOGConstructer<kmer_t, size_t_max>::construct_AC() {
 
         // Add FL to all children of nodes[index]
         for (size_t_max child_index : nodes[index].children){
-            short goto_char = get_edge_nucleotide(child_index);
+            uint8_t goto_char = get_edge_nucleotide(child_index);
 
             // Search for first node with valid goto edge
             size_t_max failure_index = nodes[index].failure;
-            while(!nodes[failure_index].has_basic_child(goto_char))
+            while(!has_basic_child[nodes[failure_index] * 4 + goto_char])
                 failure_index = nodes[failure_index].failure;
 
             // Find its child with right edge
@@ -203,36 +241,72 @@ inline void HOGConstructer<kmer_t, size_t_max>::convert_AC_to_EHOG() {
         }   
     }
 
-    // Remove non-keeped nodes
-    // Move keeped nodes to the beginning
-    std::vector<size_t_max> new_indexes(node_count, 0);
-    size_t_max keeped = 0;
-    for (size_t_max i = 0; i < node_count; ++i){
-        if (keep[i]){
-            std::swap(nodes[i], nodes[keeped++]);
-            new_indexes[i] = keeped - 1;
-        }
-    }
-    // Move all the edges to point to correct locations
-    for (size_t_max i = 0; i < keeped; ++i){
-        nodes[i].index = new_indexes[i];
-        nodes[i].parent = new_indexes[nodes[i].parent];
-        nodes[i].failure = new_indexes[nodes[i].failure];
-        
-        size_t_max child_count = nodes[i].children.size();
-        for (size_t_max j = 0; j < child_count; ++j)
-            nodes[i].children[j] = new_indexes[nodes[i].children[j]];
-    }
-    
-    // Get rid of non-keeped nodes
-    nodes.resize(keeped);
+    remove_redundant_edges(keep);
 }
 
 template <typename kmer_t, typename size_t_max>
 inline void HOGConstructer<kmer_t, size_t_max>::convert_EHOG_to_HOG() {
     if (nodes.empty) throw std::invalid_argument("Cannot convert empty EHOG to HOG");
 
+    // Initialize resulting array
     size_t_max node_count = nodes.size();
     std::vector<bool> keep(node_count, false);
     keep[0] = true;
+
+    // Initialize helper arrays
+    std::vector<bool> black(node_count, 0);
+    std::vector<uint8_t> child_count(node_count, 0);
+    std::vector<size_t_max> favorite_proper_ancestor(node_count, 0);
+    std::vector<size_t_max> favorite_descendant(node_count, 0);
+    std::vector<size_t_max> modified_vertices;
+    // Child counts
+    for (size_t_max i = 0; i < node_count; ++i) child_count[i] = nodes[i].children.size();
+    // FavPAnc
+    for (size_t_max i : BFS_indexes){
+        size_t_max proper_ancestor = nodes[i].parent;
+        if (child_count[proper_ancestor] == 1)
+            proper_ancestor = favorite_proper_ancestor[proper_ancestor];
+        
+        favorite_proper_ancestor[i] = proper_ancestor;
+    }
+    // FavDesc
+    for (size_t_max i = node_count - 1; i >= 0; --i){
+        size_t_max descendant = BFS_indexes[i];
+        if (child_count[descendant] == 1) descendant = favorite_descendant[descendant];
+        
+        favorite_descendant[BFS_indexes[i]] = descendant;
+    }
+    // Mark all leaves white
+    for (size_t_max i = 0; i < n; ++i) black[leaves[i]] = true;
+
+    for (size_t_max x : leaves){
+        keep[x] = true;
+        size_t_max v = nodes[x].failure;
+        modified_vertices.clear();
+        while (v != 0){
+            size_t_max u = favorite_descendant[v];
+            if (child_count[u] != 0){
+                child_count[u] = 0;
+                keep[x] = true;
+
+                while (u != 0){
+                    modified_vertices.push_back(u);
+                    if (u != favorite_descendant[v]){
+                        --child_count[u];
+                        if (child_count[u] != 0) break;
+                    }
+
+                    u = favorite_proper_ancestor[u];
+                }
+            }
+
+            v = nodes[v].failure;
+        }
+        if (!modified_vertices.empty()){
+            for (size_t_max v : modified_vertices) ++child_count[v];// = nodes[v].children.size();
+            child_count[modified_vertices[0]] = nodes[modified_vertices[0]].children.size();
+        }
+    }
+
+    remove_redundant_edges(keep);
 }
