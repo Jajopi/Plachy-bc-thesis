9b199c73543be65cccfc47fdf8be6928d3729b31
Ján Plachý <19651310@cuni.cz>
2025-02-13 21:55:40 +0100

    Improve testing script

--- test_parameters.sh
+++ test_parameters.sh
@@ -5,7 +5,7 @@ K="$2"
 TEST_MODE="$3"
 # "C" for counting kmers check - slow
 
-DIR="test_correctness_outputs"
+DIR="parameters_testing_outputs"
 TIME_FORMAT_STRING="time:\t%E\nmemory:\t%M"
 
 mkdir -p "$DIR";
@@ -16,43 +16,35 @@ mkdir -p "$DIR";
     ./kmercamel -p "$INPUT" -k "$K" > "$DIR"/gg_raw.txt && \
     ./kmercamel optimize -p "$DIR"/gg_raw.txt -a runs -k "$K" > "$DIR"/gg.txt
 
-# ./kmercamel -p "$INPUT" -k "$K" -c > "$DIR"/gg_c.txt
-# ./kmercamel -p "$INPUT" -k "$K" -a csac -c > "$DIR"/csac_c.txt
-
 echo =================
 
 if [ "$TEST_MODE" = "C" ]; then
     O1="$(./count_noncomplement_kmers.py -p "$DIR"/gg.txt -k "$K")"
     O2="$(./count_noncomplement_kmers.py -p "$DIR"/csac.txt -k "$K" -t)"
-    # O3="$(./count_kmers.py -p "$DIR"/gg_c.txt -k "$K")"
-    # O4="$(./count_kmers.py -p "$DIR"/csac_c.txt -k "$K" -t)"
 fi
 
-L1="$(cat "$DIR"/gg.txt | tail -n 1 | wc -m)"
-L2="$(cat "$DIR"/csac.txt | tail -n 1 | wc -m)"
-# L3="$(cat "$DIR"/gg_c.txt | tail -n 1 | wc -m)"
-# L4="$(cat "$DIR"/csac_c.txt | tail -n 1 | wc -m)"
+L1="$(cat "$DIR"/csac.txt | tail -n 1 | wc -m)"
+L2="$(cat "$DIR"/gg.txt | tail -n 1 | wc -m)"
 
-R1="$(cat "$DIR"/gg.txt | tail -n 1 | tr [a-z] '0' | tr -s '0' | tr -d [A-Z] | wc -m)"
-R2="$(cat "$DIR"/csac.txt | tail -n 1 | tr [a-z] '0' | tr -s '0' | tr -d [A-Z] | wc -m)"
-# R3="$(cat "$DIR"/gg_c.txt | tail -n 1 | tr [a-z] '0' | tr -s '0' | tr -d [A-Z] | wc -m)"
-# R4="$(cat "$DIR"/csac_c.txt | tail -n 1 | tr [a-z] '0' | tr -s '0' | tr -d [A-Z] | wc -m)"
+R1="$(cat "$DIR"/csac.txt | tail -n 1 | tr [a-z] '0' | tr -s '0' | tr -d [A-Z] | wc -m)"
+R2="$(cat "$DIR"/gg.txt | tail -n 1 | tr [a-z] '0' | tr -s '0' | tr -d [A-Z] | wc -m)"
 
-echo kmers:
-echo "$O1" gg
-echo "$O2" csac
-# echo "$O3" "gg (c)"
-# echo "$O4" "csac (c)"
+
+if [ "$TEST_MODE" = "C" ]; then
+    echo kmers:
+    printf "% 10d csac\n"   "$O1"
+    printf "% 10d gg\n"     "$O2"
+fi
+
 echo lengths:
-echo "$L1" gg
-echo "$L2" csac
-# echo "$L3" "gg (c)"
-# echo "$L4" "csac (c)"
+printf "% 10d csac\n"   "$L1"
+printf "% 10d gg\n"     "$L2"
+
 echo runs:
-echo "$R1" gg
-echo "$R2" csac
-# echo "$R3" "gg (c)"
-# echo "$R4" "csac (c)"
-echo resources:
-echo "$(cat "$DIR"/gg_time.txt)"
+printf "% 10d csac\n"   "$R1"
+printf "% 10d gg\n"     "$R2"
+
+echo resources - csac:
 echo "$(cat "$DIR"/csac_time.txt)"
+echo resources - gg:
+echo "$(cat "$DIR"/gg_time.txt)"
