712097630e219177e05f19926d5b02ac0c29a847
Ján Plachý <19651310@cuni.cz>
2025-02-23 18:57:13 +0100

    Add testing parameters

--- test_parameters.sh
+++ test_parameters.sh
@@ -11,57 +11,67 @@ TIME_FORMAT_STRING="time:\t%E\nmemory:\t%M"
 mkdir -p "$DIR";
 
 /usr/bin/time -f "$TIME_FORMAT_STRING" -o "$DIR"/csac_time.txt \
-    ./kmercamel -p "$INPUT" -k "$K" -a csac > "$DIR"/csac.txt
+    ./kmercamel -p "$INPUT" -k "$K" -a csac --precision 21 > "$DIR"/csac.txt
 /usr/bin/time -f "$TIME_FORMAT_STRING" -o "$DIR"/gg_time.txt \
     ./kmercamel -p "$INPUT" -k "$K" > "$DIR"/gg_raw.txt && \
     ./kmercamel optimize -p "$DIR"/gg_raw.txt -a runs -k "$K" > "$DIR"/gg.txt
 
-/usr/bin/time -f "$TIME_FORMAT_STRING" -o "$DIR"/csac_c_time.txt \
-    ./kmercamel -p "$INPUT" -k "$K" -a csac -c > "$DIR"/csac_c.txt
-/usr/bin/time -f "$TIME_FORMAT_STRING" -o "$DIR"/gg_c_time.txt \
-    ./kmercamel -p "$INPUT" -k "$K" -c > "$DIR"/gg_c_raw.txt && \
-    ./kmercamel optimize -p "$DIR"/gg_c_raw.txt -a runs -k "$K" > "$DIR"/gg_c.txt
+if [[ "$TEST_MODE" == *"C"* ]]; then        
+    /usr/bin/time -f "$TIME_FORMAT_STRING" -o "$DIR"/csac_c_time.txt \
+        ./kmercamel -p "$INPUT" -k "$K" -a csac -c > "$DIR"/csac_c.txt
+    /usr/bin/time -f "$TIME_FORMAT_STRING" -o "$DIR"/gg_c_time.txt \
+        ./kmercamel -p "$INPUT" -k "$K" -c > "$DIR"/gg_c_raw.txt && \
+        ./kmercamel optimize -p "$DIR"/gg_c_raw.txt -a runs -k "$K" > "$DIR"/gg_c.txt
+fi
 
 echo =================
 
-if [ "$TEST_MODE" = "C" ]; then
+if [[ "$TEST_MODE" == *"N"* ]]; then
     O1="$(./count_noncomplement_kmers.py -p "$DIR"/csac.txt -k "$K" -t)"
     O2="$(./count_noncomplement_kmers.py -p "$DIR"/gg.txt -k "$K")"
-    O3="$(./count_kmers.py -p "$DIR"/csac_c.txt -k "$K" -t)"
-    O4="$(./count_kmers.py -p "$DIR"/gg_c.txt -k "$K")"
+    if [[ "$TEST_MODE" == *"C"* ]]; then
+        O3="$(./count_kmers.py -p "$DIR"/csac_c.txt -k "$K" -t)"
+        O4="$(./count_kmers.py -p "$DIR"/gg_c.txt -k "$K")"
+    fi
 fi
 
 L1="$(cat "$DIR"/csac.txt | tail -n 1 | wc -m)"
 L2="$(cat "$DIR"/gg.txt | tail -n 1 | wc -m)"
-L3="$(cat "$DIR"/csac_c.txt | tail -n 1 | wc -m)"
-L4="$(cat "$DIR"/gg_c.txt | tail -n 1 | wc -m)"
+if [[ "$TEST_MODE" == *"C"* ]]; then
+    L3="$(cat "$DIR"/csac_c.txt | tail -n 1 | wc -m)"
+    L4="$(cat "$DIR"/gg_c.txt | tail -n 1 | wc -m)"
+fi
 
 R1="$(cat "$DIR"/csac.txt | tail -n 1 | tr [a-z] '0' | tr -s '0' | tr -d [A-Z] | wc -m)"
 R2="$(cat "$DIR"/gg.txt | tail -n 1 | tr [a-z] '0' | tr -s '0' | tr -d [A-Z] | wc -m)"
-R3="$(cat "$DIR"/csac_c.txt | tail -n 1 | tr [a-z] '0' | tr -s '0' | tr -d [A-Z] | wc -m)"
-R4="$(cat "$DIR"/gg_c.txt | tail -n 1 | tr [a-z] '0' | tr -s '0' | tr -d [A-Z] | wc -m)"
+if [[ "$TEST_MODE" == *"C"* ]]; then      
+    R3="$(cat "$DIR"/csac_c.txt | tail -n 1 | tr [a-z] '0' | tr -s '0' | tr -d [A-Z] | wc -m)"
+    R4="$(cat "$DIR"/gg_c.txt | tail -n 1 | tr [a-z] '0' | tr -s '0' | tr -d [A-Z] | wc -m)"
+fi
 
 
-if [ "$TEST_MODE" = "C" ]; then
+if [[ "$TEST_MODE" == *"N"* ]]; then
     echo kmers:
-    printf "% 10d csac\t% 10d (c)\n"   "$O1" "$O3"
-    printf "% 10d gg\t% 10d (c)\n"     "$O2" "$O4"
+    printf "% 10d\tcsac\t% 10d (c)\n"   "$O1" "$O3"
+    printf "% 10d\tgg\t% 10d (c)\n"     "$O2" "$O4"
 fi
 
 echo lengths:
-printf "% 10d csac\t% 10d (c)\n"   "$L1" "$L3"
-printf "% 10d gg\t% 10d (c)\n"     "$L2" "$L4"
+printf "% 10d\tcsac\t% 10d (c)\n"   "$L1" "$L3"
+printf "% 10d\tgg\t% 10d (c)\n"     "$L2" "$L4"
 
 echo runs:
-printf "% 10d csac\t% 10d (c)\n"   "$R1" "$R3"
-printf "% 10d gg\t% 10d (c)\n"     "$R2" "$R4"
+printf "% 10d\tcsac\t% 10d (c)\n"   "$R1" "$R3"
+printf "% 10d\tgg\t% 10d (c)\n"     "$R2" "$R4"
 
 echo resources - csac:
 echo "$(cat "$DIR"/csac_time.txt)"
 echo resources - gg:
 echo "$(cat "$DIR"/gg_time.txt)"
 
-echo "resources - csac (c)":
-echo "$(cat "$DIR"/csac_c_time.txt)"
-echo "resources - gg (c)":
-echo "$(cat "$DIR"/gg_c_time.txt)"
+if [[ "$TEST_MODE" == *"C"* ]]; then
+    echo "resources - csac (c)":
+    echo "$(cat "$DIR"/csac_c_time.txt)"
+    echo "resources - gg (c)":
+    echo "$(cat "$DIR"/gg_c_time.txt)"
+fi
