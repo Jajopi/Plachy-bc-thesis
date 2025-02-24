9a583c2b88e7ceb3c775b9624496bd146d2ea25a
Ján Plachý <19651310@cuni.cz>
2025-02-24 11:29:20 +0100

    Add printing to comparation script

--- compare.py
+++ compare.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import math
 
 INPUT_DIR = "data"
-LABELS = ["length", "runs", "time", "memory", "objective", ""]
+INPUT_FILE_NAME = "compare_inputs.txt"
+LABELS = ["length", "runs", "time", "memory", "objective", "relative obj."]
 KS = [15, 23, 31, 47, 63, 95, 127]
 
 def get_results_file_name(file_name):
@@ -40,6 +41,8 @@ def plot(file_name):
         lines = list(map(lambda x: x.strip(), file.readlines()))
     data = dict()
     for line in lines:
+        if len(line.strip()) == 0: continue
+        if not ":" in line or not ";" in line: continue
         key, value = line.split(":")
         input_name, k, complements = key.split()
         k = int(k)
@@ -51,24 +54,33 @@ def plot(file_name):
     for input_name in sorted(data.keys()):
         d = data[input_name]
         results = [list() for i in range(4)]
-        for k in d.keys():
+        for k in sorted(d.keys()):
             value = d[k]
             for i, part in enumerate(value.split(";")):
                 if len(part.strip()) == 0: continue
         
-                results[i][-1].append(list(map(lambda x: int(float(x)), part[0].split())))
+                results[i].append(list(map(lambda x: int(float(x)), part.split())))
                 results[i][-1].append(compute_objective(results[i][-1], k))
-                results[i][-1].append(0) # TODO use
+                results[i][-1].append(0)
+                results[i][-1][-1] = results[i][-1][-2] / results[0][-1][-2]
 
         fig, ax = plt.subplots(2, 3)
         fig.set_figwidth(12)
         fig.set_figheight(8)
+        fig.suptitle(input_name)
         for i, label in enumerate(LABELS):
+            # y_max = 0
+            for r in results:
+                if len(r) == 0: continue
+                y_data = list(map(lambda x: x[i], r))
+                # y_max = max(y_max, max(y_data))
+                ax[i // 3, i % 3].plot(KS[:len(r)], y_data, )
+                for x, y in zip(KS, y_data):
+                    ax[i // 3, i % 3].annotate("%s" % y, xy=(x, y), textcoords="data")
             ax[i // 3, i % 3].set_title(label)
-            ax[i // 3, i % 3].set_ylim(ymin=0)
-            for r in results[i % 2::2]:
-                ax[i // 3, i % 3].plot(KS, list(map(lambda x: x[i], r)))
+            # ax[i // 3, i % 3].set_ylim(ymin=0, ymax=y_max * 1.25)
         plt.show()
 
 if __name__ == "__main__":
-    compute("compare_inputs.txt", False)
+    # compute(INPUT_FILE_NAME, False)
+    plot(INPUT_FILE_NAME)
