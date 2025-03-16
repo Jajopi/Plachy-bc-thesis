5a9c79ca497fce5156c7e8f56832f3716f774a7a
Ján <19651310@cuni.cz>
2025-03-16 16:33:32 +0100

    Improve optimal run penalty plotting script

--- scripts/plot_optimal_run_penalty.py
+++ scripts/plot_optimal_run_penalty.py
@@ -6,7 +6,7 @@ import seaborn as sns
 import os
 
 PENALTY_SCALE = 2.5
-LAST_PENALTY_TYPE = f"log2N*{1/PENALTY_SCALE}"
+LAST_PENALTY_TYPE = f"manual"
 
 def plot():
     plt.rcParams["font.family"] = "serif"
@@ -21,6 +21,7 @@ def plot():
     alg = ALG_NEW
     for complements in (False, True):
         for inp in load_all_inputs(INPUT_FILE_NAME):
+            manual_penalty = int(inp.split()[2]) if len(inp.split()) > 2 else 0
             inp = inp.split()[0]
 
             ys, xs, yys = [], [], []
@@ -30,15 +31,15 @@ def plot():
 
                 d = results[(alg, inp, k, complements)]
                 ys.append(
-                    math.log2(d[LABELS[0]] / PENALTY_SCALE)
+                    3 + math.log2(d[LABELS[0]] / d[LABELS[1]])
                 )
                 yys.append(
-                    math.log2(d[LABELS[0]] / PENALTY_SCALE) / (3 + math.log2(d[LABELS[0]] / d[LABELS[1]]))
+                    (manual_penalty if manual_penalty != 0 else math.log2(d[LABELS[0]])) / (3 + math.log2(d[LABELS[0]] / d[LABELS[1]]))
                 )
 
             sns.lineplot(y=ys, x=xs, ax=axs[0][1 if complements else 0],
                         marker="o" if complements else "s", markeredgewidth=0,
-                        label=inp[:25])
+                        label=inp[:25] + ": " + str(manual_penalty))
             sns.lineplot(y=yys, x=xs, ax=axs[1][1 if complements else 0],
                         marker="o" if complements else "s", markeredgewidth=0,
                         label=inp[:25])
