45576447c02cc59934d0400a88eee7240e239f0d
Ján <jan.plachy946@student.cuni.cz>
2025-04-18 20:17:09 +0200

    Finish csv-creating script

--- scripts/save_to_csv.py
+++ scripts/save_to_csv.py
@@ -4,28 +4,38 @@ from compare import *
 
 CSV_SEPARATOR = ','
 
-def print_to_csv(results_or_resources=True, k=31, complements=True, subsampled=False):
+def print_to_csv(results_or_resources=True, k=31, complements=True, subsampled=False, pangenomes=False):
     results = load_all_results(RESULTS_FILE_NAME)
+
+    if subsampled and k != 31: return
 
     out_file = "outputs/"
     out_file += "results-length-runs" if results_or_resources else "results-time-memory"
-    out_file += "-" + str(k)
     if not complements: out_file += "-noncomplement"
-    if subsampled: out_file += "-subsampled"
+    if pangenomes: out_file += "-pan"
+    if subsampled: out_file += "-sub"
+    out_file += "-" + str(k)
     out_file += ".csv"
 
     with open(out_file, "w") as file:
         if results_or_resources:
             file.write(CSV_SEPARATOR.join(("dataset", "method", "length", "runs", "objective", "relative")) + '\n')
         else:
-            file.write(CSV_SEPARATOR.join(("dataset", "method", "time", "relative-time", "memory", "relative-memory")) + '\n')
+            file.write(CSV_SEPARATOR.join(("dataset", "method", "time", "relativetime", "memory", "relativememory")) + '\n')
     
         for inp in load_all_inputs(INPUT_FILE_NAME):
             run_penalty = int(inp.split()[2]) if len(inp.split()) > 2 else None
+            
             name = inp.split()[3].replace('_', ' ')
             if name == "-": continue
+            if not subsampled:
+                if "(p)" in name and not pangenomes: continue
+                if not "(p)" in name and pangenomes: continue
+            name = name.replace("(p)", "")
+
             inp = inp.split()[0]
             if ("subsampled" in inp) != subsampled: continue
+            # print(name)
             
             for alg in (reversed(ALGORITHMS)):
                 rp = run_penalty if alg != ALG_OLD else None
@@ -33,7 +43,7 @@ def print_to_csv(results_or_resources=True, k=31, complements=True, subsampled=F
                 result = results[(alg, inp, k, complements, rp)]
                 
                 if results_or_resources:
-                    file.writelines(map(str, (
+                    file.writelines(map(lambda x: str(x).strip(), (
                         name if alg != ALG_OLD else "", CSV_SEPARATOR,
                         "GGMO" if alg == ALG_OLD else f"LOAC ({rp})", CSV_SEPARATOR,
                         result[LABELS[0]], CSV_SEPARATOR,
@@ -44,7 +54,7 @@ def print_to_csv(results_or_resources=True, k=31, complements=True, subsampled=F
                 else:
                     relative_time = 1 if alg == ALG_OLD else result[LABELS[2]] / results[(ALG_OLD, inp, k, complements, None)][LABELS[2]]
                     relative_memory = 1 if alg == ALG_OLD else result[LABELS[3]] / results[(ALG_OLD, inp, k, complements, None)][LABELS[3]]
-                    file.writelines(map(str, (
+                    file.writelines(map(lambda x: str(x).strip(), (
                         name if alg != ALG_OLD else "", CSV_SEPARATOR,
                         "GGMO" if alg == ALG_OLD else "LOAC", CSV_SEPARATOR,
                         result[LABELS[2]], CSV_SEPARATOR,
@@ -57,7 +67,8 @@ def print_to_csv(results_or_resources=True, k=31, complements=True, subsampled=F
 
 if __name__ == "__main__":
     for k in (23, 31, 63):
-        print_to_csv(results_or_resources=True, k=k)
-        print_to_csv(results_or_resources=False, k=k)
-        print_to_csv(results_or_resources=True, subsampled=True, k=k)
-        print_to_csv(results_or_resources=False, subsampled=True, k=k)
+        for res_or_res in (True, False):
+            print_to_csv(results_or_resources=res_or_res, k=k, pangenomes=True)
+            print_to_csv(results_or_resources=res_or_res, k=k, pangenomes=False)
+            print_to_csv(results_or_resources=res_or_res, k=k, subsampled=True)
+            print_to_csv(results_or_resources=res_or_res, k=k)
